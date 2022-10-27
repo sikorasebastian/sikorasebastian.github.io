3eaa3584a4699720b4863dace6bbbe10feef5416
Vovul <vovul@tlen.pl>
2022-10-27 12:23:36 +0200

    initial test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UserMasterComponent } from './user-master/user-master.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserMasterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
