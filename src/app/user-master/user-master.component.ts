9dd5452240a4a3bc5b17706ea400e7c8a1f1b75c
Vovul <vovul@tlen.pl>
2022-10-27 16:53:58 +0200

    Add existing project files of dog instagram to Git

--- src/app/user-master/user-master.component.ts
+++ src/app/user-master/user-master.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-master',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-master.component.css']
 })
 export class UserMasterComponent implements OnInit {
+  dogApiList: any;
+  dogList: any;
+  imageURL: any;
+  selectedDog: any;
+  selectedDogURL: any;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+   this.dogApiList = [];
+   this.dogList = [];
+   this.imageURL = "";
+   this.selectedDog = "";
+   this.selectedDogURL = "";
+   }
 
   ngOnInit(): void {
+  this.getDogApiList()
   }
 
+getDogApiList(){
+  this.httpClient.get('https://dog.ceo/api/breeds/list/all').subscribe((result:any) =>
+  {
+  this.dogApiList = result;
+
+  for (var i in this.dogApiList.message) {
+    this.dogList.push(i);
+  }
+  })
+}
+
+selectedBreed = '';
+	onSelected(value:any): void {
+		this.selectedBreed = value.target.value;
+		this.getImages(this.selectedBreed);
+	}
+
+getImages(selectedBreed: any){
+this.httpClient.get('https://dog.ceo/api/breed/' + selectedBreed + '/images').subscribe((resultImg:any) =>
+  {
+  this.imageURL = resultImg.message[0];
+  this.selectedDog = selectedBreed;
+  this.selectedDogURL = 'https://en.wikipedia.org/wiki/'+this.selectedDog;
+  })
+}
 }
